8cdc76b6806ec0176b1c5fea53db381c528d216a
kimseowol <128126590+kimseowol@users.noreply.github.com>
2023-06-19 00:21:18 +0900

    calculator_change connect button using lambda

--- PersonalProject/calculator/calculator.py
+++ PersonalProject/calculator/calculator.py
@@ -15,55 +15,31 @@ class MyApp(QtWidgets.QMainWindow):
         self.ui = loader.load(ui_file)
         ui_file.close()
         self.ui.show()
+        
+        # connect digits 
+        self.ui.number01.clicked.connect(lambda: self.btn_number('1'))
+        self.ui.number02.clicked.connect(lambda: self.btn_number('2'))
+        self.ui.number03.clicked.connect(lambda: self.btn_number('3'))
+        self.ui.number04.clicked.connect(lambda: self.btn_number('4'))
+        self.ui.number05.clicked.connect(lambda: self.btn_number('5'))
+        self.ui.number06.clicked.connect(lambda: self.btn_number('6'))
+        self.ui.number07.clicked.connect(lambda: self.btn_number('7'))
+        self.ui.number08.clicked.connect(lambda: self.btn_number('8'))
+        self.ui.number09.clicked.connect(lambda: self.btn_number('9'))
 
-        # digit
-        self.ui.number01.clicked.connect(self.btn_number01)
-        self.ui.number02.clicked.connect(self.btn_number02)
-        self.ui.number03.clicked.connect(self.btn_number03)
-        self.ui.number04.clicked.connect(self.btn_number04)
-        self.ui.number05.clicked.connect(self.btn_number05)
-        self.ui.number06.clicked.connect(self.btn_number06)
-        self.ui.number07.clicked.connect(self.btn_number07)
-        self.ui.number08.clicked.connect(self.btn_number08)
-        self.ui.number09.clicked.connect(self.btn_number09)
+        # connect operations
+        self.ui.button_plus.clicked.connect(lambda: self.btn_operations('+'))
+        self.ui.button_sub.clicked.connect(lambda: self.btn_operations('-'))
+        self.ui.button_mult.clicked.connect(lambda: self.btn_operations('*'))
+        self.ui.button_divide.clicked.connect(lambda: self.btn_operations('/'))
+
+        # connect buttons
         self.ui.number0.clicked.connect(self.btn_number0)
         self.ui.number00.clicked.connect(self.btn_number00)
-        # operator
         self.ui.button_del.clicked.connect(self.delete_number)
         self.ui.button_dot.clicked.connect(self.dot_number)
-        self.ui.button_plus.clicked.connect(self.plus_number)
-        self.ui.button_sub.clicked.connect(self.minus_number)
-        self.ui.button_mult.clicked.connect(self.multiply_number)
-        self.ui.button_divide.clicked.connect(self.divide_number)
         self.ui.button_result.clicked.connect(self.result_number)
         self.ui.button_reset.clicked.connect(self.reset_numbers)
-
-    def btn_number01(self):
-        self.number("1")
-
-    def btn_number02(self):
-        self.number("2")
-
-    def btn_number03(self):
-        self.number("3")
-
-    def btn_number04(self):
-        self.number("4")
-
-    def btn_number05(self):
-        self.number("5")
-
-    def btn_number06(self):
-        self.number("6")
-
-    def btn_number07(self):
-        self.number("7")
-
-    def btn_number08(self):
-        self.number("8")
-
-    def btn_number09(self):
-        self.number("9")
 
     # Below . could not be processed 0000 yet
     def btn_number0(self):
@@ -80,14 +56,16 @@ class MyApp(QtWidgets.QMainWindow):
         if exist_text[-1].is_digit() or exist_text[-1] == ".":
             self.number("00")
 
-    def number(self, num):
+    def btn_operations(self, operations):
+        exist_text = self.ui.lineEdit.text()
+        if (exist_text[-1] == "+") | (exist_text[-1] == "-") | (exist_text[-1] == "/") | (exist_text[-1] == "*") | (
+                exist_text[-1] == "."):
+            self.delete_num()
+        self.btn_number(operations)
+
+    def btn_number(self, num):
         exist_text = self.ui.lineEdit.text()
         self.ui.lineEdit.setText(exist_text + num)
-
-    def is_digit(self):
-        exist_text = self.ui.lineEdit.text()
-        if (exist_text[-1] == "+") | (exist_text[-1] == "-") | (exist_text[-1] == "/") | (exist_text[-1] == "*") | (exist_text[-1] == "."):
-            self.delete_number()
 
     def delete_number(self):
         exist_text = self.ui.lineEdit.text()
@@ -113,22 +91,6 @@ class MyApp(QtWidgets.QMainWindow):
             pass
         else:
             self.ui.lineEdit.setText(exist_text+".")
-
-    def plus_number(self):
-        self.is_digit()
-        self.number("+")
-
-    def minus_number(self):
-        self.is_digit()
-        self.number("-")
-
-    def multiply_number(self):
-        self.is_digit()
-        self.number("*")
-
-    def divide_number(self):
-        self.is_digit()
-        self.number("/")
 
     def result_number(self):
         exist_text = self.ui.lineEdit.text()
